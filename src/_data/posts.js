c0e35c7cc015d18377af386832a07ad7dc01557e
Andrés Del Carpio <andresdc@andresdc.net>
2022-11-20 21:25:41 -0500

    Refactor request to accomodate json file

--- src/_data/posts.js
+++ src/_data/posts.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
 module.exports = async () => {
-  const posts = await axios.get('https://adcpe.github.io/blog-content/postlist.json')
-  return posts.data.data
+  const response = await axios.get('https://adcpe.github.io/blog-content/postlist.json')
+  return response.data.posts
 }
