82d09a0cd7bc0a9ff49d7e1f3a3a845c0d427f70
Andrés Del Carpio <andresdc@andresdc.net>
2022-11-21 00:19:15 -0500

    Changed folder structure, added navbar

---
+++ src/_data/links.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { name: 'Home', to: '/' },
+  { name: 'About', to: '/about' },
+  { name: 'Blog', to: '/blog' }
+  // { name: 'Projects', to: '/projects' },
+  // { name: 'FAQ', to: '/faq' }
+]
