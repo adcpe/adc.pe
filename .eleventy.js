68f1d2e2cd075b303fc3ebca6eef3cdb59c01f44
Andrés Del Carpio <andresdc@andresdc.net>
2022-11-21 11:15:18 -0500

    Modified addPassThroughCopy for css

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = (eleventyConfig) => {
   eleventyConfig.addWatchTarget('./src/assets/stylus/')
-  eleventyConfig.addPassthroughCopy('./src/assets/css/')
+  eleventyConfig.addPassthroughCopy({'./assets/css/': 'css'})
 
   return {
     dir: {
