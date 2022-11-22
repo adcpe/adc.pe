2bf529b64172831cd8af2d9062674c7284448fab
Andrés Del Carpio <andresdc@andresdc.net>
2022-11-21 22:40:56 -0500

    Format dates

--- src/_data/posts.js
+++ src/_data/posts.js
@@ -1,6 +1,8 @@
 const axios = require('axios')
+const formatDate = require('../utils/formatDate')
 
 module.exports = async () => {
   const response = await axios.get('https://adcpe.github.io/blog-content/postlist.json')
+  response.data.posts.forEach((el) => (el.data.date = formatDate(el.data.date)))
   return response.data.posts
 }
