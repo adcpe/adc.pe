179e3c44eefb35fc97d7cb0d355491ef95796372
Andrés Del Carpio <adc@adc.pe>
2023-01-12 16:19:55 -0500

    Remove GitLab link

--- src/_data/footerLinks.js
+++ src/_data/footerLinks.js
@@ -3,10 +3,10 @@ module.exports = [
     name: 'Github',
     class: 'fa-github',
     to: 'https://github.com/adcpe'
-  },
-  {
-    name: 'Gitlab',
-    class: 'fa-gitlab',
-    to: 'https://gitlab.com/adcpe'
   }
+  // {
+  //   name: 'Gitlab',
+  //   class: 'fa-gitlab',
+  //   to: 'https://gitlab.com/adcpe'
+  // }
 ]
