336f7bdbc90d4ae012b66ea6319b1bd66766f94b
Andrés Del Carpio <adc@adc.pe>
2023-02-09 11:48:31 -0500

    Added more footer links

--- src/_data/footerLinks.js
+++ src/_data/footerLinks.js
@@ -1,12 +1,32 @@
 module.exports = [
   {
+    name: 'Email',
+    class: 'fa-at',
+    to: 'mailto:adc@adc.pe'
+  },
+  {
     name: 'Github',
     class: 'fa-github',
     to: 'https://github.com/adcpe'
-  }
+  },
   // {
   //   name: 'Gitlab',
   //   class: 'fa-gitlab',
   //   to: 'https://gitlab.com/adcpe'
-  // }
+  // },
+  {
+    name: 'LinkedIn',
+    class: 'fa-linkedin',
+    to: 'https://www.linkedin.com/in/adcpe/'
+  },
+  {
+    name: 'Mastodon',
+    class: 'fa-mastodon',
+    to: 'https://hachyderm.io/@adcpe'
+  },
+  {
+    name: 'Last.fm',
+    class: 'fa-lastfm',
+    to: 'https://www.last.fm/user/adc39'
+  }
 ]
