9e6436e622d0118e07ac47690e26c0c9be53c500
Andrés Del Carpio <adc@adc.pe>
2025-04-22 21:12:05 -0500

    Update footer links

--- src/_data/footerLinks.js
+++ src/_data/footerLinks.js
@@ -9,24 +9,9 @@ module.exports = [
     class: 'fa-github',
     to: 'https://github.com/adcpe'
   },
-  // {
-  //   name: 'Gitlab',
-  //   class: 'fa-gitlab',
-  //   to: 'https://gitlab.com/adcpe'
-  // },
   {
     name: 'LinkedIn',
     class: 'fa-linkedin',
     to: 'https://www.linkedin.com/in/adcpe/'
-  },
-  {
-    name: 'Mastodon',
-    class: 'fa-mastodon',
-    to: 'https://hachyderm.io/@adcpe'
-  },
-  {
-    name: 'Last.fm',
-    class: 'fa-lastfm',
-    to: 'https://www.last.fm/user/adc39'
   }
 ]
