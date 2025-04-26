93f7d044b4d9f7f6f78d4200394184b108d89374
Andrés Del Carpio <adc@adc.pe>
2025-04-26 15:40:51 -0500

    Revert changes

--- src/_data/posts.js
+++ src/_data/posts.js
@@ -4,6 +4,6 @@ import formatDate from '../utils/formatDate.js'
 export default async () => {
   const response = await axios.get('https://adcpe.github.io/blog-content/postlist.json')
   response.data.posts.forEach((el) => (el.data.date = formatDate(el.data.date)))
-  response.data.posts.forEach((el) => (console.log(el)))
+
   return response.data.posts
 }
