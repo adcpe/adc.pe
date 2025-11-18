e278089b96d087581fe2143883192c7c086330bb
Andrés Del Carpio <adc@adc.pe>
2025-11-18 12:55:37 -0500

    Change blog permalinks

--- src/utils/renameFolder.js
+++ src/utils/renameFolder.js
@@ -1,7 +1,7 @@
 import { rename } from 'fs'
 
-const oldFolderPath = './dist/blog/{{ pagination.pageNumber }}-{{ post.data.slug }}' // Current folder name
-const newFolderPath = './dist/blog/0-2020-07-08-rendering-markdown-on-react' // New folder name
+const oldFolderPath = './dist/blog/{{ post.data.id }}' // Current folder name
+const newFolderPath = './dist/blog/0' // New folder name
 
 export default () => {
   rename(oldFolderPath, newFolderPath, (err) => {
